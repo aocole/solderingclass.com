08454827fff1e40ab21d2d1011309488209b4e9c
Andrew Cole <aocole@gmail.com>
2024-12-11 23:20:33 -0800

    astro/tailwind version of website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import tailwind from '@astrojs/tailwind'
+
+import icon from 'astro-icon'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://solderingclass.com',
+  integrations: [tailwind(), icon()],
+})
